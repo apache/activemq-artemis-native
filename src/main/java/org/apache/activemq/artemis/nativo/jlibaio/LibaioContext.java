75bd61d966d4617ed34fd61ffe9c6b03564b93ce
Robbie Gemmell <robbie@apache.org>
2022-07-12 14:02:16 +0100

    adjust steps so that the .h is always [re]generated before cmake runs in regular mvn builds

--- src/main/java/org/apache/activemq/artemis/nativo/jlibaio/LibaioContext.java
+++ src/main/java/org/apache/activemq/artemis/nativo/jlibaio/LibaioContext.java
@@ -44,9 +44,11 @@ import org.slf4j.LoggerFactory;
  * <a href="https://ext4.wiki.kernel.org/index.php/Clarifying_Direct_IO's_Semantics">Interesting reading for this.</a>
  */
 public class LibaioContext<Callback extends SubmitInfo> implements Closeable {
-   /* Notice: After making changes to the native interface, you have to use mvn install at least once to generate the include file.
+   /* Notice: After making changes to the native interface, mvn 'generate-sources' must occur at least once to generate the updated include file.
       This is because the maven compiler plugin is the one generating org_apache_activemq_artemis_native_jlibaio_LibaioContext.h
-      So that file needs to be updated before Cmake comes along to compile the module. */
+      So that file needs to be updated before Cmake comes along to compile the module.
+      This normally happens as needed in the regular mvn build, so if you use e.g 'mvn clean install -Ppodman' then no extra step is needed,
+      specific attention is only required if you e.g run the build scripts directly yourself. */
 
    private static final Logger logger = LoggerFactory.getLogger(LibaioContext.class);
 
